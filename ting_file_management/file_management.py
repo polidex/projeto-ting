b84092eb417fee43332e25eea0c76f25e170ad89
polidex <lp.igor@gmail.com>
2023-04-11 13:47:03 -0300

    feat: reqt 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+from pathlib import Path
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    path = Path(path_file)
+    if path.suffix != '.txt':
+        sys.stderr.write('Formato inválido')
+
+    try:
+        with open(path_file) as file:
+            data = file.read().split('\n')
+            return data
+    except OSError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
