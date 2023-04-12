947c5aa2f498011b679989343d4abb78e877d1ad
polidex <lp.igor@gmail.com>
2023-04-12 20:40:04 -0300

    feat: reqt 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,21 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    for i in range(0, len(instance)):
+        file = {
+            "palavra": word,
+            "arquivo": instance.search(i)['nome_do_arquivo'],
+            "ocorrencias": []
+        }
+        for index in range(0, len(instance.search(i)['linhas_do_arquivo'])):
+            row = instance.search(i)['linhas_do_arquivo'][index]
+            if word.lower() in row.lower():
+                file['ocorrencias'].append({"linha": i + 1, "conteudo": row})
+
+        print(len(file['ocorrencias']))
+
+        result = []
+        if len(file['ocorrencias']) > 0:
+            result.append(file)
+
+    return result
